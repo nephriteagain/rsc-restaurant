014ec494510b2ce4332473541f22ff120c10941c
nephriteagain <oneabove232@gmail.com>
2023-11-07 21:46:17 +0800

    minor

--- components/promos/PromoDetails..tsx
+++ components/promos/PromoDetails..tsx
@@ -10,7 +10,7 @@ export default function PromoDetails({
 
     return (
         <div className="w-full flex flex-row gap-8 flex-wrap items-center justify-center">
-            <div  className="w-[90vw] sm:w-[500px] md:w-[400px] lg:w-[300px] aspect-[100/161] rounded-2xl shadow-lg bg-secondaryLight" />
+            <div  className="w-[90vw] sm:w-[500px] md:w-[400px] lg:w-[300px] aspect-[100/130] rounded-2xl shadow-lg bg-secondaryLight" />
             <div className="flex flex-col gap-4 min-w-[300px] max-w-[600px]">
                 <h2 className="font-bold text-4xl">{title}</h2>
                 <p className="leading-10 border border-gray-400 px-2 py-4 text-lg">{desc}</p>
